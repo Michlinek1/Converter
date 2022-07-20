0b04001d9face7c69cde03ecdad8d07b022033c9
Michal <Michal@MICHLINEK>
2022-07-20 15:58:52 +0200

    Add project files.

---
+++ Converter/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxCelc = new System.Windows.Forms.TextBox();
+            this.textBoxFahre = new System.Windows.Forms.TextBox();
+            this.convertbutton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBoxhide = new System.Windows.Forms.CheckBox();
+            this.buttonzmiana = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxCelc
+            // 
+            this.textBoxCelc.Location = new System.Drawing.Point(660, 235);
+            this.textBoxCelc.Multiline = true;
+            this.textBoxCelc.Name = "textBoxCelc";
+            this.textBoxCelc.PlaceholderText = "Celc";
+            this.textBoxCelc.Size = new System.Drawing.Size(216, 28);
+            this.textBoxCelc.TabIndex = 2;
+            this.textBoxCelc.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBoxFahre
+            // 
+            this.textBoxFahre.Location = new System.Drawing.Point(660, 432);
+            this.textBoxFahre.Multiline = true;
+            this.textBoxFahre.Name = "textBoxFahre";
+            this.textBoxFahre.PlaceholderText = "Fahre";
+            this.textBoxFahre.ReadOnly = true;
+            this.textBoxFahre.Size = new System.Drawing.Size(216, 28);
+            this.textBoxFahre.TabIndex = 3;
+            this.textBoxFahre.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxFahre.TextChanged += new System.EventHandler(this.textBoxFahre_TextChanged);
+            // 
+            // convertbutton
+            // 
+            this.convertbutton.Location = new System.Drawing.Point(678, 521);
+            this.convertbutton.Name = "convertbutton";
+            this.convertbutton.Size = new System.Drawing.Size(181, 72);
+            this.convertbutton.TabIndex = 4;
+            this.convertbutton.Text = "Convert!";
+            this.convertbutton.UseVisualStyleBackColor = true;
+            this.convertbutton.Click += new System.EventHandler(this.convertbutton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Ostanie konwercje:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // checkBoxhide
+            // 
+            this.checkBoxhide.AutoSize = true;
+            this.checkBoxhide.Location = new System.Drawing.Point(12, 125);
+            this.checkBoxhide.Name = "checkBoxhide";
+            this.checkBoxhide.Size = new System.Drawing.Size(137, 19);
+            this.checkBoxhide.TabIndex = 7;
+            this.checkBoxhide.Text = "Hide last convertions";
+            this.checkBoxhide.UseVisualStyleBackColor = true;
+            this.checkBoxhide.CheckedChanged += new System.EventHandler(this.checkBoxhide_CheckedChanged);
+            // 
+            // buttonzmiana
+            // 
+            this.buttonzmiana.Location = new System.Drawing.Point(727, 297);
+            this.buttonzmiana.Name = "buttonzmiana";
+            this.buttonzmiana.Size = new System.Drawing.Size(84, 71);
+            this.buttonzmiana.TabIndex = 8;
+            this.buttonzmiana.Text = "⇅";
+            this.buttonzmiana.UseVisualStyleBackColor = true;
+            this.buttonzmiana.Click += new System.EventHandler(this.buttonzmiana_click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(568, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(369, 151);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Converter";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, -1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 34);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Open errors file";
+            this.button1.UseMnemonic = false;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1463, 824);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonzmiana);
+            this.Controls.Add(this.checkBoxhide);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.convertbutton);
+            this.Controls.Add(this.textBoxFahre);
+            this.Controls.Add(this.textBoxCelc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private TextBox textBoxCelc;
+        private TextBox textBoxFahre;
+        private Button convertbutton;
+        private Label label3;
+        private CheckBox checkBoxhide;
+        private Button buttonzmiana;
+        private Label label1;
+        private Button button1;
+    }
+}
---
+++ Converter/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System.Diagnostics;
+namespace Converter
+
+{
+    /*
+     TODO:
+          Poprawić wygląd
+          
+     
+    */
+    public partial class Form1 : Form
+    {
+        private bool isHidden = false;
+
+        public string ustawienia = File.ReadAllText(@"C:\Users\" + Environment.UserName + @"\Documents\temperatura\ustawienia.txt");
+
+        
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            if (ustawienia.Contains("Schowany: false"))
+            {
+                checkBoxhide.Checked = false;
+                this.label3.Visible = true;
+               
+
+            }
+            else
+            {
+                checkBoxhide.Checked = true;
+                this.label3.Visible = false;
+            }
+            var tekst = File.ReadAllText(@"C:\Users\"+ Environment.UserName+@"\Documents\temperatura\temeperatura.txt");
+            if (new FileInfo(fileName: @"C:\Users\"+Environment.UserName+@"\Documents\temperatura\temeperatura.txt").Length == 0)
+            {
+                return;
+            }
+            this.label3.Text = "Ostatnie konwercje:" + "\n" + tekst;
+           
+
+
+
+
+
+        }
+
+        private void convertbutton_Click(object sender, EventArgs e)
+        {
+            File.ReadAllText(@"C:\Users\"+ Environment.UserName+@"\Documents\temperatura\temeperatura.txt");
+            var textboxcelc = textBoxCelc.Text;
+            var textboxfahre = textBoxFahre.Text;
+            var historia = label3.Text;
+            
+
+            try
+            {
+                if (this.textBoxCelc.ReadOnly == false)
+                {
+                    var converter = Convert.ToDouble(textboxcelc) *1.8 + 32;
+                    var wiadomosc = textBoxFahre.Text = converter.ToString() + "F";
+                    this.label3.Text += wiadomosc + " " + DateTime.Now.ToString("HH:mm:ss tt") + "\n";
+                    Creator(wiadomosc, historia);
+                }
+                else
+                {
+                    var converter = (Convert.ToDouble(textboxfahre)- 32) * 5/9;
+                    var wiadomosc = textBoxCelc.Text = converter.ToString() + "C";
+                    this.label3.Text += wiadomosc + " " + DateTime.Now.ToString("HH:mm:ss tt") + "\n";
+                    Creator(wiadomosc, historia);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Błąd", "Wystąpił błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                var f1 = new FileStream(@"C:\Users\"+Environment.UserName+@"\Documents\temperatura\bledy.txt", FileMode.Append);
+                using (StreamWriter sw = new StreamWriter(f1))
+                {
+                    sw.Write(ex + "\n");
+                    sw.Close();
+                }
+            }
+
+
+
+
+
+
+
+
+        }
+
+        private void Creator(string wiadomosc, string historia)
+        {
+            try
+            {
+                var f1 = new FileStream(@"C:\Users\"+ Environment.UserName+@"\Documents\temperatura\temeperatura.txt", FileMode.Append);
+                using (StreamWriter sw = new StreamWriter(f1))
+                {
+                    sw.Write(wiadomosc + " " + DateTime.Now.ToString("HH:mm:ss tt") +  "\n");
+                    sw.Close();
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Błąd", "Wystąpił błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                var f1 = new FileStream(@"C:\Users\"+ Environment.UserName+@"\Documents\temperatura\bledy.txt", FileMode.Append);
+                using (StreamWriter sw = new StreamWriter(f1))
+                {
+                    sw.Write(ex + "\n");
+                    sw.Close();
+                }
+            }
+
+        }
+
+
+        private void checkBoxhide_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxhide.Checked)
+            {
+                this.label3.Visible = false;
+                var zmianaReplace = ustawienia.Replace("Schowany: false", "Schowany: true");
+                File.WriteAllText(@"C:\Users\"+ Environment.UserName+@"\Documents\temperatura\ustawienia.txt", zmianaReplace);
+
+
+
+
+            }
+            else
+            {
+                this.label3.Visible = true;
+                var zmianaReplace = ustawienia.Replace("Schowany: true", "Schowany: false");
+                File.WriteAllText(@"C:\Users\"+ Environment.UserName+@"\Documents\temperatura\ustawienia.txt", zmianaReplace);
+                
+            }
+        }
+
+        private void buttonzmiana_click(object sender, EventArgs e)
+        {
+            this.textBoxCelc.Text = "";
+            this.textBoxFahre.Text = "";
+            if (this.textBoxCelc.ReadOnly == false)
+            {
+               
+                this.textBoxCelc.ReadOnly = true;
+                this.textBoxFahre.ReadOnly = false;
+                
+            }
+            else
+            {
+                this.textBoxCelc.ReadOnly = false;
+                this.textBoxFahre.ReadOnly = true;
+            }
+        }
+
+        private void textBoxFahre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("notepad.exe", @"C:\Users\" + Environment.UserName + @"\Documents\temperatura\bledy.txt");
+        }
+    }
+}
